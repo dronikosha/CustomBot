95555272d7a488b293ce8d0ee349bf44b480ac27
dronikosha <dronikosha@gmail.com>
2022-07-23 13:54:35 +0300

    vk request

--- CustomBot/main/views.py
+++ CustomBot/main/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render, redirect, HttpResponse
+import requests
+from environs import Env
+
+env = Env()
+env.read_env()
 
 
 # Create your views here.
 def index(request):
-    return render(request, 'main/index.html')
+    # Димин айди стоит
+    data = {"access_token": env.str("VK_BOT_KEY"), "user_id": 146653997, "message": "Это тебе написал бот с сайта. Дима лох", "v": "5.131", "random_id": 0}
+    if request.method == "POST":
+        r = requests.post("https://api.vk.com/method/messages.send", data=data)
+        print(r.status_code, r.reason)
+        print("test")
+    return render(request, 'main/base.html')
 
 
 def about(request):
-    return render(request, 'main/about.html')
+    return render(request, 'main/base.html')
+
+
+def make_request(request):
+    return render(request, 'main/base.html')
